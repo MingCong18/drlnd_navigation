2de125aa35ac9df7b1c048707f7a2c49a3aa81fe
Ming Cong <ming.cong188@gmail.com>
2018-12-26 12:13:29 -0800

    add ddqn to dqn

--- dqn_agent.py
+++ dqn_agent.py
@@ -20,17 +20,20 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, args, seed):
         """Initialize an Agent object.
         
         Params
         ======
             state_size (int): dimension of each state
             action_size (int): dimension of each action
+            args (object): parameters
             seed (int): random seed
         """
         self.state_size = state_size
         self.action_size = action_size
+        self.ddqn = args.ddqn
+        self.dueling = args.dueling
         self.seed = random.seed(seed)
 
         # Q-Network
@@ -86,7 +89,12 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        if self.ddqn:
+            next_state_actions = self.qnetwork_local(next_states).max(1)[1]
+            Q_targets_next = self.qnetwork_target(next_states).detach().\
+                gather(1, next_state_actions.unsqueeze(-1))
+        else:
+            Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
