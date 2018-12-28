1ed3464e99c7a35dde3d8328280cd0200e5aca08
Ming Cong <ming.cong188@gmail.com>
2018-12-28 13:21:52 -0800

    put replay buffer classes into a new file

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,8 +1,8 @@
 import numpy as np
 import random
-from collections import namedtuple, deque
 
 from model import QNetwork, DuelingDQN
+from replay_buffer import ReplayBuffer, PrioReplayBuffer
 
 import torch
 import torch.nn.functional as F
@@ -37,11 +37,16 @@ class Agent():
         self.prio = args.prio
         self.seed = random.seed(seed)
 
-        if self.dueling: # if we train dueling DQN
+        if self.ddqn:
+            print("Double DQN Enabled!")
+        else:
+            print("Double DQN Not Enabled!")
+
+        if self.dueling: # when dueling is enable we initialize dueling DQN
             print("Dueling DQN Enabled!")
             self.qnetwork_local = DuelingDQN(state_size, action_size, seed).to(device)
             self.qnetwork_target = DuelingDQN(state_size, action_size, seed).to(device)
-        else:
+        else: # else we initialize the basic DQN
             print("Dueling DQN Not Enabled!")
             self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
             self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
@@ -55,13 +60,9 @@ class Agent():
         else:
             print("Prioritized Replay Buffer Not Enabled!")
             self.memory = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE, seed)
+
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
-
-        if self.ddqn:
-            print("Double DQN Enabled!")
-        else:
-            print("Double DQN Not Enabled!")
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -92,8 +93,6 @@ class Agent():
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
             if len(self.memory) > BATCH_SIZE:
-                #experiences, experiences_indices, experiences_weights = self.memory.sample(beta)
-                #self.learn(experiences, GAMMA)
                 self.learn(GAMMA)
 
     def learn(self, gamma):
@@ -112,6 +111,9 @@ class Agent():
             states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
+        # When double DQN is enabled, we calculate the best action to take in the next state
+        # using our local trained network, but values corresponding to this action come
+        # from the target network
         if self.ddqn:
             next_state_actions = self.qnetwork_local(next_states).max(1)[1]
             Q_targets_next = self.qnetwork_target(next_states).detach().\
@@ -125,6 +127,9 @@ class Agent():
         Q_expected = self.qnetwork_local(states).gather(1, actions)
 
         # Compute loss
+        # When prioritized replay buffer is enabled, we calculate the MSE manually.
+        # This allows us to take into account weights of sampesla dn keep individual
+        # loss values for every sample
         if self.prio:
             losses_v = batch_weights_v * (Q_expected - Q_targets) ** 2
             sample_prios_v = losses_v + 1e-5
@@ -132,6 +137,7 @@ class Agent():
             self.memory.update_priorities(batch_indices, sample_prios_v.data.cpu().numpy().squeeze(-1))
         else:
             loss = F.mse_loss(Q_expected, Q_targets)
+            
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
@@ -154,110 +160,3 @@ class Agent():
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
 
-class ReplayBuffer:
-    """Fixed-size buffer to store experience tuples."""
-
-    def __init__(self, buffer_size, batch_size, seed):
-        """Initialize a ReplayBuffer object.
-
-        Params
-        ======
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
-            seed (int): random seed
-        """
-        self.memory = deque(maxlen=buffer_size)  
-        self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
-    
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-    
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-  
-        return (states, actions, rewards, next_states, dones)
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)
-
-
-class PrioReplayBuffer:
-    """Prioritized Replay Buffer."""
-
-    def __init__(self, buffer_size, batch_size, seed):
-        """Initialize a Prioritized ReplayBuffer object.
-
-        Params
-        ======
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
-            seed (int): random seed
-        """
-        self.memory = []
-        self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
-        self.priorities = np.zeros((buffer_size, ), dtype=np.float32)
-        self.pos = 0
-        self.prob_alpha = 0.6
-        self.beta_start = 0.4
-        self.beta_frames = 100000
-        self.beta_idx = 0
-        self.capacity = buffer_size
-    
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        # append priorities
-        max_prio = self.priorities.max() if self.memory else 1.0
-        if len(self.memory) < self.capacity:
-            self.memory.append(e)
-        else:
-            self.memory[self.pos] = e
-        self.priorities[self.pos] = max_prio
-        self.pos = (self.pos + 1) % self.capacity
-    
-    def sample(self):
-        if len(self.memory) == self.capacity:
-            prios = self.priorities
-        else:
-            prios = self.priorities[:self.pos]
-        probs = prios ** self.prob_alpha
-        probs /= probs.sum()
-        # convert priorities to probabilities
-        indices = np.random.choice(len(self.memory), self.batch_size, p=probs)
-        experiences = [self.memory[idx] for idx in indices]
-        # calculate weights
-        total = len(self.memory)
-        beta = min(1.0, self.beta_start + self.beta_idx * (1.0-self.beta_start)/self.beta_frames)
-        self.beta_idx += 1
-        weights = (total * probs[indices]) ** (-beta)
-        weights /= weights.max()
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-
-        return (states, actions, rewards, next_states, dones), indices, weights
-
-    def update_priorities(self, batch_indices, batch_priorities):
-        for idx, prio in zip(batch_indices, batch_priorities):
-            self.priorities[idx] = prio
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)
---
+++ replay_buffer.py
@@ -0,0 +1,116 @@
+from collections import namedtuple, deque
+import random
+import numpy as np
+
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.memory = deque(maxlen=buffer_size)  
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+  
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+class PrioReplayBuffer:
+    """Prioritized Replay Buffer."""
+
+    def __init__(self, buffer_size, batch_size, seed):
+        """Initialize a Prioritized ReplayBuffer object.
+
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.memory = []
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+        self.priorities = np.zeros((buffer_size, ), dtype=np.float32)
+        self.pos = 0
+        self.prob_alpha = 0.6
+        self.beta_start = 0.4
+        self.beta_frames = 100000
+        self.beta_idx = 0
+        self.capacity = buffer_size
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        # append priorities
+        max_prio = self.priorities.max() if self.memory else 1.0
+        if len(self.memory) < self.capacity:
+            self.memory.append(e)
+        else:
+            self.memory[self.pos] = e
+        self.priorities[self.pos] = max_prio
+        self.pos = (self.pos + 1) % self.capacity
+    
+    def sample(self):
+        if len(self.memory) == self.capacity:
+            prios = self.priorities
+        else:
+            prios = self.priorities[:self.pos]
+        probs = prios ** self.prob_alpha
+        probs /= probs.sum()
+        # convert priorities to probabilities
+        indices = np.random.choice(len(self.memory), self.batch_size, p=probs)
+        experiences = [self.memory[idx] for idx in indices]
+        # calculate weights
+        total = len(self.memory)
+        beta = min(1.0, self.beta_start + self.beta_idx * (1.0-self.beta_start)/self.beta_frames)
+        self.beta_idx += 1
+        weights = (total * probs[indices]) ** (-beta)
+        weights /= weights.max()
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones), indices, weights
+
+    def update_priorities(self, batch_indices, batch_priorities):
+        for idx, prio in zip(batch_indices, batch_priorities):
+            self.priorities[idx] = prio
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
